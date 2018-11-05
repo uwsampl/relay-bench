a878c0db2684dcc0fbb95d804b9d7682018d8def
Marisa Kirisame <lolisa@marisa.moe>
2018-11-05 12:57:51 -0800

    run only once

--- rnn/relay/__init__.py
+++ rnn/relay/__init__.py
@@ -1,3 +1,2 @@
 # from . import char_rnn_classifier
-from . import char_rnn_generator
-from . import util
+# from . import char_rnn_generator
--- rnn/relay/util.py
+++
@@ -1,3 +0,0 @@
-from . import char_rnn_classifier
-from . import char_rnn_generator
-from . import util
