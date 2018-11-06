2494d4f4ce8ac9638a849c4e6072347c5484d3ce
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-06 14:29:23 -0800

    nnvm -> relay in util

--- tvm_benchmark/util.py
+++ tvm_benchmark/util.py
@@ -1,7 +1,7 @@
 """Utility for benchmark"""
 
 import sys
-import nnvm
+from tvm import relay
 
 def get_network(name, batch_size, dtype='float32'):
     """Get the symbol definition and random weight of a network
@@ -30,38 +30,38 @@ def get_network(name, batch_size, dtype='float32'):
     output_shape = (batch_size, 1000)
 
     if name == 'mobilenet':
-        net, params = nnvm.testing.mobilenet.get_workload(batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.mobilenet.get_workload(batch_size=batch_size, dtype=dtype)
     elif name == 'mobilenet_v2':
-        net, params = nnvm.testing.mobilenet_v2.get_workload(batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.mobilenet_v2.get_workload(batch_size=batch_size, dtype=dtype)
     elif name == 'inception_v3':
         input_shape = (batch_size, 3, 299, 299)
-        net, params = nnvm.testing.inception_v3.get_workload(batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.inception_v3.get_workload(batch_size=batch_size, dtype=dtype)
     elif "resnet" in name:
         n_layer = int(name.split('-')[1])
-        net, params = nnvm.testing.resnet.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.resnet.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
     elif "vgg" in name:
         n_layer = int(name.split('-')[1])
-        net, params = nnvm.testing.vgg.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.vgg.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
     elif "densenet" in name:
         n_layer = int(name.split('-')[1])
-        net, params = nnvm.testing.densenet.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
+        net, params = relay.testing.densenet.get_workload(num_layers=n_layer, batch_size=batch_size, dtype=dtype)
     elif "squeezenet" in name:
         version = name.split("_v")[1]
-        net, params = nnvm.testing.squeezenet.get_workload(batch_size=batch_size, version=version, dtype=dtype)
+        net, params = relay.testing.squeezenet.get_workload(batch_size=batch_size, version=version, dtype=dtype)
     elif name == 'custom':
         # an example for custom network
-        from nnvm.testing import utils
-        net = nnvm.sym.Variable('data')
-        net = nnvm.sym.conv2d(net, channels=4, kernel_size=(3,3), padding=(1,1))
-        net = nnvm.sym.flatten(net)
-        net = nnvm.sym.dense(net, units=1000)
-        net, params = utils.create_workload(net, batch_size, (3, 224, 224), dtype=dtype)
-    elif name == 'mxnet':
-        # an example for mxnet model
-        from mxnet.gluon.model_zoo.vision import get_model
-        block = get_model('resnet18_v1', pretrained=True)
-        net, params = nnvm.frontend.from_mxnet(block)
-        net = nnvm.sym.softmax(net)
+        from tvm.relay.testing import init
+        net = relay.var('data')
+        net = relay.testing.layers.conv2d(net, channels=4, kernel_size=(3,3), padding=(1,1))
+        net = relay.nn.batch_flatten(net)
+        net = relay.testing.layers.dense_add_bias(net, units=1000)
+        net, params = init.create_workload(net, batch_size, (3, 224, 224))
+    # elif name == 'mxnet':
+    #     # an example for mxnet model
+    #     from mxnet.gluon.model_zoo.vision import get_model
+    #     block = get_model('resnet18_v1', pretrained=True)
+    #     net, params = nnvm.frontend.from_mxnet(block)
+    #     net = nnvm.sym.softmax(net)
     else:
         raise ValueError("Unsupported network: " + name)
 
