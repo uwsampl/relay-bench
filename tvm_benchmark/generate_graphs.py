cca09b847ac7abb0b1c0ebdfd25064728a4a6494
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-09 11:09:26 -0800

    push-button from models to csv

--- tvm_benchmark/generate_graphs.py
+++ tvm_benchmark/generate_graphs.py
@@ -3,9 +3,10 @@ import shlex
 import itertools
 
 MODELS = [
-    "mlp",
+    # "mlp",
     "dqn",
     # "resnet-18",
+    # "mobilenet",
     ]
 
 IRS = ["relay", "nnvm"]
@@ -18,8 +19,12 @@ def main():
     with open(OUT_FILE, "w") as outf:
         print("ir, model, avg time (ms), std dev (ms)", file=outf)
 
+    subprocess.Popen(shlex.split("python3 -m tvm.exec.rpc_server --tracker 0.0.0.0:9190 --key foo"))
+    
+    subprocess.Popen(shlex.split("python3 -m tvm.exec.rpc_tracker --port 9190"))
+
     for ir, model in itertools.product(IRS, MODELS):
-        subprocess.call(["python3", "tvm_benchmark/x86_cpu_imagenet_bench.py",
+        subprocess.run(["python3", "tvm_benchmark/x86_cpu_imagenet_bench.py",
                         "--ir", ir,
                         "--network", model,
                         "--rpc-key", "foo",
