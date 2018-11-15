b6c85171e92b0489cdb153e84e84d0c5c203b325
Marisa Kirisame <lolisa@marisa.moe>
2018-11-15 01:51:59 -0800

    save

--- bench_rnn.py
+++ bench_rnn.py
@@ -27,13 +27,10 @@ def bench_forward(input_size, hidden_size, output_size, iterations=1000):
     relay_loop_start = time.time()
     relay_rnn = relay.char_rnn_generator.RNNLoop(data.N_LETTERS, hidden_size, data.N_LETTERS)
     for i in range(iterations):
-        # Relay using RNN list
-        # relay_rnn.samples('Russian', 'RUS')
-        # relay_rnn.samples('German', 'GER')
-        # relay_rnn.samples('Spanish', 'SPA')
-        # relay_rnn.samples('Chinese', 'CHI')
-        # print("time of relay: " + time_since(relay_loop_start))
-        pass
+        relay_rnn.samples('Russian', 'RUS')
+        relay_rnn.samples('German', 'GER')
+        relay_rnn.samples('Spanish', 'SPA')
+        relay_rnn.samples('Chinese', 'CHI')
 
     pytorch_start = time.time()
     for i in range(iterations):
@@ -47,7 +44,7 @@ def bench_forward(input_size, hidden_size, output_size, iterations=1000):
 
 def main():
     # cProfile.run('bench_forward(data.N_LETTERS, N_HIDDEN, data.N_LETTERS, 50)')
-    bench_forward(data.N_LETTERS, N_HIDDEN, data.N_LETTERS, 1)
+    bench_forward(data.N_LETTERS, N_HIDDEN, data.N_LETTERS, 10)
 
 if __name__ == "__main__":
     main()
