a11513c8b7e8962b2f1970f5720063ae95a7d073
Josh Maxwell Pollock <joshpoll@cs.washington.edu>
2018-11-16 14:39:31 -0800

    visualization modifications including adding vta

--- tvm_benchmark/visualize.py
+++ tvm_benchmark/visualize.py
@@ -5,21 +5,22 @@ import argparse
 import os
 from shutil import copyfile
 
-def visualize(outdir="graph"):
+def visualize(outdir="graph", spec="tvm_benchmark/graph.json"):
     # prepare outdir
     if not os.path.exists(outdir):
         os.makedirs(outdir)
 
     # vega-lite -> svg
-    subprocess.run(shlex.split(f"vl2svg tvm_benchmark/graph.json {outdir}/graph.svg"))
+    subprocess.run(shlex.split(f"vl2svg {spec} {outdir}/graph.svg"))
 
     # svg -> pdf
     cairosvg.svg2pdf(url=f"{outdir}/graph.svg", write_to=f"{outdir}/graph.pdf")
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument("--spec", type=str, default="tvm_benchmark/graph.json")
     parser.add_argument("--outdir", type=str, default="graph",
                         help="The output directory of the pdf.")
     args = parser.parse_args()
 
-    visualize(args.outdir)
+    visualize(args.outdir, args.spec)
