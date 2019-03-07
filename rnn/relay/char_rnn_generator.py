d2e04125caeaf8e0a54d7569cfe74e51894622d3
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-03-06 17:46:51 -0800

    Update references to aot and Relay AST construction

--- rnn/relay/char_rnn_generator.py
+++ rnn/relay/char_rnn_generator.py
@@ -14,8 +14,8 @@ from tvm import relay
 from tvm.relay import op
 from tvm.relay import create_executor, Module
 from tvm.relay.prelude import Prelude
-from aot import aot
 from network import *
+import aot
 
 class RNNCellOnly(Network):
     def compute(self, input_size, hidden_size, output_size):
@@ -23,10 +23,10 @@ class RNNCellOnly(Network):
         self.input_var = inp = relay.var('input', shape=(1, input_size))
         self.hidden_var = hidden = relay.var('hidden', shape=(1, hidden_size))
         self.hidden = initialize(self.hidden_var)
-        combined = op.concatenate2(op.concatenate2(category, inp, axis=1), hidden, axis=1)
+        combined = op.concatenate([op.concatenate([category, inp], axis=1), hidden], axis=1)
         hidden = self.linear(N_CATEGORIES + input_size + hidden_size, hidden_size, combined, name='i2h')
         output = self.linear(N_CATEGORIES + input_size + hidden_size, output_size, combined, name='i2o')
-        output_combined = op.concatenate2(hidden, output, axis=1)
+        output_combined = op.concatenate([hidden, output], axis=1)
         output = self.linear(hidden_size + output_size, output_size, output_combined, name='o2o')
         #output = op.nn.dropout(output, 0.1) #dropout isnt simplified, commented out for now
         output = op.nn.log_softmax(output, axis=1)
@@ -42,10 +42,10 @@ class RNNLoop(Network):
         one_diag = relay.const(np.diag(np.ones(58)).astype('float32'))
         boxed_one = relay.const(np.array([1]).astype('int32'))
         inp = op.take(one_diag, op.multiply(boxed_one, inp_topi), axis=0)
-        combined = op.concatenate2(op.concatenate2(category, inp, axis=1), hidden, axis=1)
+        combined = op.concatenate([op.concatenate([category, inp], axis=1), hidden], axis=1)
         hidden = self.linear(N_CATEGORIES + input_size + hidden_size, hidden_size, combined, name='i2h')
         output = self.linear(N_CATEGORIES + input_size + hidden_size, output_size, combined, name='i2o')
-        output_combined = op.concatenate2(hidden, output, axis=1)
+        output_combined = op.concatenate([hidden, output], axis=1)
         output = self.linear(hidden_size + output_size, output_size, output_combined, name='o2o')
         # output = op.nn.dropout(output, 0.1) #attributes has not been registered
         output = op.nn.log_softmax(output, axis=1)
@@ -60,9 +60,11 @@ class RNNLoop(Network):
         max = relay.var('max', shape=(), dtype='int32')
         inp_para = [max] + [copy_var(v) for v in fwd_para]
         fwd_res = self.fwd(*inp_para[1:])
-        fwd_res_1 = fwd_res[1]
-        else_else_branch = self.prelude.cons(fwd_res_1, self.recurse(op.subtract(max, relay.const(1)), inp_para[1], fwd_res_1, fwd_res[0]))
-        else_branch = relay.If(fwd_res[2], self.prelude.nil(), else_else_branch)
+        fwd_res_0 = relay.TupleGetItem(fwd_res, 0)
+        fwd_res_1 = relay.TupleGetItem(fwd_res, 1)
+        fwd_res_2 = relay.TupleGetItem(fwd_res, 2)
+        else_else_branch = self.prelude.cons(fwd_res_1, self.recurse(op.subtract(max, relay.const(1)), inp_para[1], fwd_res_1, fwd_res_0))
+        else_branch = relay.If(fwd_res_2, self.prelude.nil(), else_else_branch)
         body = relay.If(op.equal(max, relay.const(0)), self.prelude.nil(), else_branch)
         return inp_para, relay.Let(self.fwd, fwd_func, body), None
 
