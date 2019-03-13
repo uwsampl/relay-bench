4b9c6bb01f157b0f161d29e083f5d6bba53499c4
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-03-13 16:56:28 -0700

    Add mobilenet v2 for pytorch

--- oopsla_benchmarks/pytorch/__init__.py
+++ oopsla_benchmarks/pytorch/__init__.py
@@ -1,1 +1,2 @@
 from . import util
+from mobilenetv2 import MobileNetV2
--- oopsla_benchmarks/pytorch/util/util.py
+++ oopsla_benchmarks/pytorch/util/util.py
@@ -5,14 +5,21 @@ import time
 import tvm
 
 import torchvision.models as models
+from util.mobilenetv2 import PRETRAIN_PARAMS as MOBILENET_PARAMS
+from util.mobilenetv2.MobileNetV2 import MobileNetV2
 
-def instantiate_network(network, batch_size):
+def instantiate_network(network, batch_size, dev):
     image_shape = (224, 3, 224, batch_size)
 
     if network == 'resnet-18':
         net = models.resnet18(pretrained=True)
     elif network == 'vgg-16':
         net = models.vgg16(pretrained=True)
+    elif network == 'mobilenet-v2':
+        net = MobileNetV2(n_class=1000)
+        loc = MOBILENET_PARAMS
+        state_dict = torch.load(loc) if dev != 'cpu' else torch.load(loc, map_location='cpu')
+        net.load_state_dict(state_dict)
 
     return (net, image_shape)
 
@@ -27,7 +34,7 @@ def evaluate_model(net, image_shape, device):
 
 
 def score(network, dev, batch_size, num_batches):
-    net, image_shape = instantiate_network(network, batch_size)
+    net, image_shape = instantiate_network(network, batch_size, dev)
 
     device = torch.device('cuda' if dev == 'gpu' and torch.cuda.is_available() else 'cpu')
 
