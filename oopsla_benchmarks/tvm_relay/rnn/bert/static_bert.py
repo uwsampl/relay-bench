924f020f7b58e39bd7eb0b4fb126ac6d857ec60c
Jared Roesch <roeschinc@gmail.com>
2019-03-29 02:21:05 -0700

    Add work on porting Gluon Bert model

---
+++ oopsla_benchmarks/tvm_relay/rnn/bert/static_bert.py
@@ -0,0 +1,18 @@
+from tvm import relay
+import mxnet as mx
+from mxnet import gluon
+
+net = gluon.nn.SymbolBlock.imports(
+    "gluon_export/bert_static.json",
+    ["data0", "data1", "data2"],
+    "gluon_export/bert_static.params")
+
+data0 = mx.sym.Variable('data0')
+data1 = mx.sym.Variable('data1')
+data2 = mx.sym.Variable('data2')
+expr, params = relay.frontend.from_mxnet(
+    net,
+    shape={'data0': (24, 384), 'data1': (24, 384), 'data2': (24,) },
+    input_symbols=[data0, data1, data2])
+
+import pdb; pdb.set_trace()
