45255ebc5a98876cafbdbab05fe2105f92817bc6
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-05-24 16:09:50 -0700

    Script fixes and copy webpage to /share

--- dashboard/dashboard_job.sh
+++ dashboard/dashboard_job.sh
@@ -11,17 +11,30 @@ script_dir=$(pwd)
 rm -rf ~/dashboard-tvm
 git clone --recursive https://github.com/dmlc/tvm ~/dashboard-tvm
 mkdir ~/dashboard-tvm/build
-cp dashboard/config.cmake ~/dashboard-tvm/build
+cp config.cmake ~/dashboard-tvm/build
 cd ~/dashboard-tvm; make -j 32
 
 # have to set the newly-pulled tvm to be the one called from python
 export TVM_HOME=~/dashboard-tvm
 export PYTHONPATH=$TVM_HOME/python:$TVM_HOME/topi/python:$TVM_HOME/nnvm/python:${PYTHONPATH}
 
+# ensure relay AOT will be on the Python path
+export PYTHONPATH=/share/relay-aot:${PYTHONPATH}
+
+# ensure CUDA will be present in the path
+export PATH=/usr/local/cuda-10.0/bin:/usr/local/cuda-10.0/NsightCompute-1.0${PATH:+:${PATH}}
+
+# need CUDA LD libraries too
+export LD_LIBRARY_PATH=/usr/local/cuda-10.0/lib64${LD_LIBRARY_PATH:+:${LD_LIBRARY_PATH}}
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/extras/CUPTI/lib64
+
+
 # make a timestamped directory to copy all data and graphs over to
 datestr="$(date +"%m-%d-%Y-%H%M")"
+share_store_path="/share/benchmarks"
 bundle_dir_path="/var/tmp/benchmarks_$datestr"
-mkdir "$bundle_dir_path"
+mkdir -p "$share_store_path"
+mkdir -p "$bundle_dir_path"
 echo "storing bundle in \"$bundle_dir_path\""
 
 # move to parent directory of this script
@@ -42,3 +55,4 @@ cp jerry.jpg "${bundle_dir_path}"
 
 # generate static website in bundle to view its data
 python3 gen_webpage.py "--out-dir=${bundle_dir_path}"
+cp -r $bundle_dir_path $share_store_path
