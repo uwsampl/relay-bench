ba7cd0038b146b1937b415e66951de41d03436b1
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-02 11:48:05 -0700

    Slack integration now pings users when benchmarks fail

--- dashboard/slack_integration.py
+++ dashboard/slack_integration.py
@@ -2,6 +2,7 @@
 Slack webhook that is passed in.'''
 import argparse
 import json
+import math
 import os
 import requests
 import sys
@@ -46,11 +47,35 @@ def tree_lstm_text_summary(data):
     return ret
 
 
+def nans_present(data, fields):
+    for field in fields:
+        for (_, val) in data[field].items():
+            if not isinstance(val, dict):
+                if math.isnan(val):
+                    return True
+                continue
+            for (_, time) in val.items():
+                if math.isnan(time):
+                    return True
+    return False
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('--data-dir', type=str, default='')
+    parser.add_argument('--ping-users', type=str, default='',
+                        help='Comma-separated list of user IDs to ping if there is a problem.')
     parser.add_argument('--post-webhook', type=str, required=True)
     args = parser.parse_args()
+
+    benchmarks = {
+        'opt_cpu': ('Relay Optimization Level Comparisons (CPU)', relay_opt_text_summary),
+        'opt_gpu': ('Relay Optimization Level Comparisons (GPU)', relay_opt_text_summary),
+        'cnn_cpu': ('CNN Comparisons (CPU)', cnn_text_summary),
+        'cnn_gpu': ('CNN Comparisons (GPU)', cnn_text_summary),
+        'char-rnn': ('Char RNN Comparison (CPU)', char_rnn_text_summary),
+        'treelstm': ('TreeLSTM Comparison (CPU)', tree_lstm_text_summary)
+    }
 
     with open(os.path.join(args.data_dir, 'data.json')) as json_file:
         data = json.load(json_file)
@@ -61,35 +86,29 @@ if __name__ == '__main__':
             'pretext': 'Dashboard data after run on {}. Times are in ms.'.format(data['timestamp']),
             'fields': [
                 {
-                    'title': 'Relay Optimization Level Comparisons (CPU)',
-                    'value': relay_opt_text_summary(data['opt_cpu']),
-                    'short': False
-                },
-                {
-                    'title': 'Relay Optimization Level Comparisons (GPU)',
-                    'value': relay_opt_text_summary(data['opt_gpu']),
-                    'short': False
-                },
-                {
-                    'title': 'CNN Comparisons (CPU)',
-                    'value': cnn_text_summary(data['cnn_cpu']),
-                    'short': False
-                },
-                {
-                    'title': 'CNN Comparisons (GPU)',
-                    'value': cnn_text_summary(data['cnn_gpu']),
-                    'short': False
-                },
-                {
-                    'title': 'Char RNN Comparison (CPU)',
-                    'value': char_rnn_text_summary(data['char-rnn']),
-                    'short': False
-                },
-                {
-                    'title': 'TreeLSTM Comparison (CPU)',
-                    'value': tree_lstm_text_summary(data['treelstm']),
+                    'title': title,
+                    'value': summary(data[field]),
                     'short': False
                 }
+                for (field, (title, summary)) in benchmarks.items()
             ]
         }]
         r = requests.post(args.post_webhook, json=message)
+
+        # ping if there's a NaN in the data
+        if nans_present(data, benchmarks.keys()):
+            users = args.ping_users.split(',')
+            if not users:
+                # no point in writing a warning if no one will be pinged
+                sys.exit()
+            pings = ', '.join(['<@{}>'.format(user) for user in users])
+            message = {
+                'text': 'Attention {}: something is broken!'.format(pings),
+                'attachments': [{
+                    'color': '#fa0000',
+                    'title': 'Failing benchmarks',
+                    'text': ', '.join([title for (field, (title, _)) in benchmarks.items() if nans_present(data, [field])]),
+                    'fields': []
+                }]
+            }
+            r = requests.post(args.post_webhook, json=message)
