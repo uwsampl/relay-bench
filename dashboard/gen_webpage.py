50883d4b9d7120ad33cc1d66770563b8d9614ec7
Logan Weber <weberlo@cs.washington.edu>
2019-07-05 17:57:10 -0700

    Add deadline countdown to generated webpage

--- dashboard/gen_webpage.py
+++ dashboard/gen_webpage.py
@@ -16,10 +16,56 @@ PAGE_PREFIX = '''
 <body bgcolor="ffffff" link="006666" alink="8b4513" vlink="006666" style="background-image: url(%s); background-position: center;">
 <div align="center">
 <div align="center"><h1 style="background-color: white;">TVM Relay Dashboard</h1></div>
+<div align="center"><h2 style="background-color: white; color: red;">ASPLOS Abstract Deadline: <span id="asplos-2020-abstract-deadline"></span></h1></div>
+<div align="center"><h2 style="background-color: white; color: red;">ASPLOS Paper Deadline: <span id="asplos-2020-paper-deadline"></span></h1></div>
 '''
 
 PAGE_SUFFIX = '''
 </div>
+<script>
+function elem(id) {
+  return document.getElementById(id);
+}
+
+function install_countdown(nm, tgt) {
+  // update every second
+  var x = setInterval(function() {
+    var diff = tgt - (new Date().getTime());
+
+    var d = Math.floor(diff / (1000 * 60 * 60 * 24));
+    var h = Math.floor((diff % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var m = Math.floor((diff % (1000 * 60 * 60)) / (1000 * 60));
+    var s = Math.floor((diff % (1000 * 60)) / 1000);
+
+    var tm = "";
+    if(d > 0) {
+      tm = d + " days";
+    } else if(h > 0) {
+      tm = h + " hours";
+    } else if(m > 0) {
+      tm = m + " minutes";
+    } else if(s > 0) {
+      tm = s + " seconds";
+    } else {
+      tm = tgt + " passed";
+    }
+    elem(nm).innerHTML = tm;
+
+    if(diff < 0) {
+      // how can this work if x not in scope?!
+      // https://www.w3schools.com/howto/howto_js_countdown.asp
+      clearInterval(x);
+    }
+  }, 1000);
+}
+
+window.onload = function(e) {
+  install_countdown('asplos-2020-abstract-deadline',
+    new Date("Aug 9, 2019 23:59:59").getTime());
+  install_countdown('asplos-2020-paper-deadline',
+    new Date("Aug 16, 2019 23:59:59").getTime());
+}
+</script>
 </body>
 </html>
 '''
