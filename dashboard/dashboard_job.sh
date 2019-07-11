be5ea7587ca7c178d481ec4c6c8af36ea8aead22
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-11 14:41:46 -0700

    Remove unnecessary dependency from dashboard_job

--- dashboard/dashboard_job.sh
+++ dashboard/dashboard_job.sh
@@ -8,10 +8,6 @@ tmp_scripts=~/tmp_dashboard_scripts
 rm -rf $tmp_scripts
 git clone git@github.com:uwsampl/relay-bench.git $tmp_scripts
 
-# need to set up treelstm data
-cd $tmp_scripts/oopsla_benchmarks/pytorch/rnn/tlstm
-./fetch_and_preprocess.sh
-
 # run its run script
 cd $tmp_scripts/dashboard
 ./run_dashboard.sh
