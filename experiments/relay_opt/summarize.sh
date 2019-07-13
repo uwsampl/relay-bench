ccc8ce4cf7b2ed6c67c629ca4b713ec37da44d8c
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-12 17:59:12 -0700

    Add summarization script for Relay opt level comparison

---
+++ experiments/relay_opt/summarize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+config_dir=$1
+data_dir=$2
+dest_dir=$3
+
+source $BENCHMARK_DEPS/bash/common.sh
+
+current_dir=$script_dir("$0")
+export PYTHONPATH=$BENCHMARK_DEPS/python:${PYTHONPATH}
+export PYTHONPATH=$current_dir:${PYTHONPATH}
+
+python3 relay_summarize.py --config-dir $config_dir --data-dir $data_dir --output-dir $dest_dir
