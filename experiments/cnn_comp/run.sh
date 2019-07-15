6bdeb3ecfe88f8e062e304a893a9538c55829f38
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-15 13:16:34 -0700

    Factor out Python script wrapper

--- experiments/cnn_comp/run.sh
+++ experiments/cnn_comp/run.sh
@@ -7,21 +7,8 @@ source $BENCHMARK_DEPS/bash/common.sh
 include_shared_python_deps
 add_to_pythonpath $(pwd)
 
-# first arg: script
-# second: config dir
-# third: data dir
-# Runs the script and exits if it has a nonzero error code;
-# this way, each script overwrites the status but the script
-# will stop if any fails (and writes a failing status)
-function run_framework {
-    python3 "$1" --config-dir "$2" --output-dir "$3"
-    if [ $? -ne 0 ]; then
-        exit 1;
-    fi
-}
-
-run_framework "run_relay.py" $config_dir $data_dir
-run_framework "run_nnvm.py" $config_dir $data_dir
-run_framework "run_mxnet.py" $config_dir $data_dir
-run_framework "run_tf.py" $config_dir $data_dir
-run_framework "run_pt.py" $config_dir $data_dir
+python_run_trial "run_relay.py" $config_dir $data_dir
+python_run_trial "run_nnvm.py" $config_dir $data_dir
+python_run_trial "run_mxnet.py" $config_dir $data_dir
+python_run_trial "run_tf.py" $config_dir $data_dir
+python_run_trial "run_pt.py" $config_dir $data_dir
