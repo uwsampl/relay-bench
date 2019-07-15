bc88aa2544592f39a3db78641d4a4640c3569fc0
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-15 13:16:34 -0700

    Factor out Python script wrapper

--- shared/bash/common.sh
+++ shared/bash/common.sh
@@ -24,3 +24,21 @@ function include_shared_python_deps {
 }
 export -f include_shared_python_deps
 
+# Runs a Python script with the passed arguments and exits if its exit
+# code is nonzero
+function check_python_exit_code {
+    python3 "$@"
+    if [ $? -ne 0 ]; then
+        exit 1;
+    fi
+}
+export -f check_python_exit_code
+
+# Takes three arguments (a Python script meant to run a trial, a directory meant
+# to be the config dir, and a directory meant to be the output dir) and runs the
+# following: python3 script.py --config-dir config_dir --output-dir output_dir
+# Exits if the exit code is nonzero
+function python_run_trial {
+    check_python_exit_code "$1" "--config-dir" "$2" "--output-dir" "$3"
+}
+export -f python_run_trial
