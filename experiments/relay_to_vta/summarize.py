ef7cfebbf6d4f2861546c9692b8388c66791975f
Logan Weber <weberlo@cs.washington.edu>
2019-07-25 22:14:14 -0700

    Add real Relay-to-VTA summary

--- experiments/relay_to_vta/summarize.py
+++ experiments/relay_to_vta/summarize.py
@@ -1,7 +1,11 @@
 import argparse
+from decimal import Decimal
 
 from validate_config import validate
-from common import write_status, write_summary
+from common import write_status, write_summary, sort_data
+
+SIM_TARGETS = {'sim', 'tsim'}
+PHYS_TARGETS = {'pynq'}
 
 def main(data_dir, config_dir, output_dir):
     config, msg = validate(config_dir)
@@ -9,8 +13,23 @@ def main(data_dir, config_dir, output_dir):
         write_status(output_dir, False, msg)
         return
 
-    write_summary(output_dir, config['title'], 'SUMMARY')
+    all_data = sort_data(data_dir)
+    most_recent = all_data[-1]
+    summary = ''
+
+    for target in most_recent.keys() & SIM_TARGETS:
+        summary = '_Stats on {}_\n'.format(target.upper())
+        for (stat, val) in most_recent[target].items():
+            summary += '{}: {:.2E}\n'.format(stat, Decimal(val))
+
+    for target in most_recent.keys() & PHYS_TARGETS:
+        data = most_recent[target]
+        summary += '_Time on {}_: {} ± {}\n'.format(
+            target.upper(), data['mean'], data['std_dev'])
+
+    write_summary(output_dir, config['title'], summary)
     write_status(output_dir, True, 'success')
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
