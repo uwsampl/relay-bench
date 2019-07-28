7f82095af712d3b094464e44282b5b42d2c0bced
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-07-28 13:59:42 -0700

    Apparently not enough to set CUDA_VISIBLE_DEVICES in Python, something weird about Cron

--- experiments/cnn_comp/run.sh
+++ experiments/cnn_comp/run.sh
@@ -10,5 +10,8 @@ add_to_pythonpath $(pwd)
 python_run_trial "run_relay.py" $config_dir $data_dir
 python_run_trial "run_nnvm.py" $config_dir $data_dir
 python_run_trial "run_mxnet.py" $config_dir $data_dir
+# tensorflow needs this variable set, not sure why it isn't
+# sufficient to set this within Python
+export CUDA_VISIBLE_DEVICES=0
 python_run_trial "run_tf.py" $config_dir $data_dir
 python_run_trial "run_pt.py" $config_dir $data_dir
