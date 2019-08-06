b8ea9eebed3e6c5e4396f673393d0a7d59e3c713
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-08-05 17:25:20 -0700

    Corrections to script wrappers

--- experiments/char_rnn/run.sh
+++ experiments/char_rnn/run.sh
@@ -8,7 +8,7 @@ include_shared_python_deps
 add_to_pythonpath $(pwd)
 
 # this is to initialize any language data in case it's missing
-wrap_script_status $data_dir python3 language_data.py
+wrap_command_status "$data_dir" python3 language_data.py
 
 python_run_trial "run_pt.py" $config_dir $data_dir
 python_run_trial "run_relay.py" $config_dir $data_dir
