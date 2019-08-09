060a557d803ed4a8aa73743fe59da6da13270a96
Logan Weber <weberlo@cs.washington.edu>
2019-08-09 10:37:51 -0700

    Switch over to Matplotlib styles

--- shared/python/plot_util.py
+++ shared/python/plot_util.py
@@ -15,16 +15,17 @@ from common import prepare_out_file, gather_stats, traverse_fields
 
 BAR_LABEL_Y_COEFF = 1.05
 BAR_WIDTH = 0.2
-MULTI_BAR_Y_TOP_PAD_COEFF = 1.17
 MIN_NUM_Y_TICKS = 3
 TARGET_NUM_Y_TICKS = 5
 MAX_NUM_Y_TICKS = 10
 GRID_COLOR = '#e8e8e8'
 # matplotlib default palette is ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
 # '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
-COLOR_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#bcbd22', '#17becf']
+COLOR_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+#COLOR_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#bcbd22', '#17becf']
 LINEAR_AXIS_STEPS = [0.001, 0.01, 0.1, 0.25, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
 LOG_AXIS_BASES = [2, 5, 10]
+PLOT_STYLE = 'seaborn'
 
 class PlotType(enum.Enum):
     # Bar plot with a single bar per x tick
@@ -34,6 +35,15 @@ class PlotType(enum.Enum):
     # Standard line graph
     LINE = 2
 
+    def is_bar_variant(self):
+        return self in {PlotType.BAR, PlotType.MULTI_BAR}
+
+
+Y_TOP_PAD_COEFF = {
+    PlotType.BAR: 1.1,
+    PlotType.MULTI_BAR: 1.17,
+    PlotType.LINE: 1.0
+}
 
 class PlotScale(enum.Enum):
     LINEAR = 0
@@ -50,15 +60,18 @@ class PlotBuilder:
     def __init__(self):
         self.bar_width = BAR_WIDTH
         self.figsize = plt.rcParams['figure.figsize']
+        self.style = plt.style.context(PLOT_STYLE)
 
     def make(self, plot_type, data):
+        self.style.__enter__()
+
         self.plot_type = plot_type
 
         # change default appearance
         plt.rcParams['font.family'] = ['Roboto', 'DejaVu Sans']
         plt.rcParams['figure.titleweight'] = 'bold'
         plt.rcParams['lines.linewidth'] = 2
-        plt.rcParams['axes.prop_cycle'] = cycler('color', COLOR_PALETTE)
+        #plt.rcParams['axes.prop_cycle'] = cycler('color', COLOR_PALETTE)
 
         fig, ax = plt.subplots(figsize=self.figsize)
         self.fig = fig
@@ -87,7 +100,6 @@ class PlotBuilder:
             if self.y_scale == PlotScale.LOG:
                 self.ax.get_xaxis().get_major_formatter().labelOnlyBase = False
         plt.minorticks_off()
-        format_ms(self.ax)
         self._set_up_y_axis(y_data)
 
         return self
@@ -168,7 +180,7 @@ class PlotBuilder:
         if isinstance(x_data[0], datetime.datetime):
             plt.gcf().autofmt_xdate()
 
-        self.ax.grid(True, color=GRID_COLOR, zorder=0)
+        #self.ax.grid(True, color=GRID_COLOR, zorder=0)
 
         return y_data
 
@@ -180,9 +192,9 @@ class PlotBuilder:
         return list(np.array([min_y if np.isnan(y) else y for y in y_data]) * 1e3)
 
     def _post_bar_setup(self, y_data):
-        # only use a grid on the y axis
-        self.ax.xaxis.grid(False)
-        self.ax.yaxis.grid(True, color=GRID_COLOR, zorder=0)
+        ## only use a grid on the y axis
+        #self.ax.xaxis.grid(False)
+        #self.ax.yaxis.grid(True, color=GRID_COLOR, zorder=0)
         # disable x-axis ticks (but show labels) by setting the tick length to 0
         self.ax.tick_params(axis='both', which='both',length=0)
 
@@ -257,6 +269,12 @@ class PlotBuilder:
                 raise RuntimeError('no suitable log base could be found for visualization')
             return best_base, best_base_start, best_base_stop
 
+        def _format_sub_one(value, tick_position):
+            return '{:3.1f}'.format(value)
+
+        def _format_int(value, tick_position):
+            return '{}'.format(int(value))
+
         y_data = list(y_data)
 
         if not hasattr(self, 'y_scale') or self.y_scale == PlotScale.LINEAR:
@@ -264,7 +282,7 @@ class PlotBuilder:
         else:
             y_scale = self.y_scale
 
-        if self.plot_type == PlotType.BAR or self.plot_type == PlotType.MULTI_BAR:
+        if self.plot_type.is_bar_variant():
             # we want to have the bottom of the bars as low as possible on bar plots
             if y_scale == PlotScale.LOG:
                 # can't use 0 on log-scale plots
@@ -277,24 +295,33 @@ class PlotBuilder:
         if y_scale == PlotScale.LINEAR:
             step, start, stop = _choose_linear_step(y_min, y_max)
             if step is not None:
+                if step < 1.0:
+                    formatter = _format_sub_one
+                else:
+                    formatter = _format_int
                 self.ax.set_yticks([i for i in np.arange(start, stop, step)])
         elif y_scale == PlotScale.LOG:
             base, start, stop = _choose_log_base(y_min, y_max)
+            formatter = _format_int
             if base is not None:
-                self.ax.set_yticks([base**i for i in range(start, stop+1)])
+                self.ax.set_yticks([int(base**i) for i in range(start, stop+1)])
 
-        if self.plot_type == PlotType.MULTI_BAR:
+        if self.plot_type.is_bar_variant():
+            y_top_pad_coeff = Y_TOP_PAD_COEFF[self.plot_type]
             # add padding on the top to make room for bar labels
             if y_scale == PlotScale.LOG:
-                y_max = np.power(base, (np.log(y_max) / np.log(base)) * MULTI_BAR_Y_TOP_PAD_COEFF)
+                y_max = np.power(base, (np.log(y_max) / np.log(base)) * y_top_pad_coeff)
             else:
-                y_max *= MULTI_BAR_Y_TOP_PAD_COEFF
+                y_max *= y_top_pad_coeff
             self.ax.set_ylim([y_min, y_max])
+
+        self.ax.yaxis.set_major_formatter(FuncFormatter(formatter))
 
 
     def save(self, dirname, filename):
         outfile = prepare_out_file(dirname, filename)
         plt.savefig(outfile, dpi=500, bbox_inches='tight')
+        self.style.__exit__(None, None, None)
         plt.close()
 
     def set_title(self, title):
@@ -324,13 +351,6 @@ class PlotBuilder:
     def set_figsize(self, figsize):
         self.figsize = figsize
         return self
-
-
-def format_ms(ax):
-    def milliseconds(value, tick_position):
-        return '{:3.1f}'.format(value)
-    formatter = FuncFormatter(milliseconds)
-    ax.yaxis.set_major_formatter(formatter)
 
 
 def generate_longitudinal_comparisons(sorted_data, output_dir,
