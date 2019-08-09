206026f00d80229aa40e622e1ff0eceff302f01f
Logan Weber <weberlo@cs.washington.edu>
2019-08-09 14:20:35 -0700

    Make RtV linear scale

--- experiments/relay_to_vta/visualize.py
+++ experiments/relay_to_vta/visualize.py
@@ -40,7 +40,7 @@ def generate_arm_vta_comparisons(data, output_prefix):
     for (target, target_plot_data) in plot_data.items():
         filename = 'arm-vta-{}.png'.format(target)
         PlotBuilder().set_y_label('Inference Time (ms)') \
-                     .set_y_scale(PlotScale.LOG) \
+                     .set_y_scale(PlotScale.LINEAR) \
                      .make(PlotType.MULTI_BAR, target_plot_data) \
                      .save(comparison_dir, filename)
 
