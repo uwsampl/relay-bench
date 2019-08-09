8f4413257af7fa7a20e0296c86198e8e3f5c7c0f
Logan Weber <weberlo@cs.washington.edu>
2019-08-08 19:28:56 -0700

    Fix longitudinal graph Y scales
    And change the color palette

--- shared/python/plot_util.py
+++ shared/python/plot_util.py
@@ -7,6 +7,7 @@ import os
 import numpy as np
 
 import matplotlib.pyplot as plt
+from matplotlib import cycler
 from matplotlib.ticker import FuncFormatter
 from matplotlib.font_manager import FontProperties
 
@@ -15,10 +16,14 @@ from common import prepare_out_file, gather_stats, traverse_fields
 BAR_LABEL_Y_COEFF = 1.05
 BAR_WIDTH = 0.2
 MULTI_BAR_Y_TOP_PAD_COEFF = 1.17
-NUM_Y_TICKS = 5
+MIN_NUM_Y_TICKS = 3
+TARGET_NUM_Y_TICKS = 5
+MAX_NUM_Y_TICKS = 10
 GRID_COLOR = '#e8e8e8'
-PLOT_STYLE = 'seaborn-paper'
-LINEAR_AXIS_STEPS = [1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
+# matplotlib default palette is ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728',
+# '#9467bd', '#8c564b', '#e377c2', '#7f7f7f', '#bcbd22', '#17becf']
+COLOR_PALETTE = ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd', '#bcbd22', '#17becf']
+LINEAR_AXIS_STEPS = [0.001, 0.01, 0.1, 0.25, 0.5, 1, 2, 5, 10, 20, 50, 100, 200, 500, 1000]
 LOG_AXIS_BASES = [2, 5, 10]
 
 class PlotType(enum.Enum):
@@ -44,19 +49,20 @@ class PlotScale(enum.Enum):
 class PlotBuilder:
     def __init__(self):
         self.bar_width = BAR_WIDTH
-        self.num_y_ticks = NUM_Y_TICKS
-        self.style = PLOT_STYLE
         self.figsize = plt.rcParams['figure.figsize']
 
     def make(self, plot_type, data):
         self.plot_type = plot_type
 
+        # change default appearance
+        plt.rcParams['font.family'] = ['Roboto', 'DejaVu Sans']
+        plt.rcParams['figure.titleweight'] = 'bold'
+        plt.rcParams['lines.linewidth'] = 2
+        plt.rcParams['axes.prop_cycle'] = cycler('color', COLOR_PALETTE)
+
         fig, ax = plt.subplots(figsize=self.figsize)
         self.fig = fig
         self.ax = ax
-        # TODO(weberlo): it doesn't seem like the `context` call is influencing
-        # the style
-        #with plt.style.context(self.style):
 
         # plot-type-specific config
         if plot_type == PlotType.BAR:
@@ -65,11 +71,6 @@ class PlotBuilder:
             y_data = self._make_multi_bar(data)
         elif plot_type == PlotType.LINE:
             y_data = self._make_line(data)
-
-        # change default appearance
-        plt.rcParams['font.family'] = ['Roboto', 'DejaVu Sans']
-        plt.rcParams['figure.titleweight'] = 'bold'
-        plt.rcParams['lines.linewidth'] = 2
 
         if hasattr(self, 'title'):
             plt.title(self.title, fontweight='bold')
@@ -114,17 +115,15 @@ class PlotBuilder:
         offset = 0
         bars = []
 
-        for framework_data in data.values():
-            # TODO(weberlo): handle unit conversions better
-            # seconds to milliseconds
-            single_y_data = self._process_y_data(list(framework_data.values()))
+        for i, framework_data in enumerate(data.values()):
+            framework_y_data = self._process_y_data(list(framework_data.values()))
             bar = self.ax.bar(
                     positions + offset,
-                    single_y_data,
+                    framework_y_data,
                     self.bar_width,
                     zorder=3)
             offset += self.bar_width
-            self._label_bar_val(bar, single_y_data)
+            self._label_bar_val(bar, framework_y_data)
             bars.append(bar)
         if not bars:
             return
@@ -175,8 +174,10 @@ class PlotBuilder:
 
     def _process_y_data(self, y_data):
         # TODO(weberlo): handle unit conversions better
-        # seconds to milliseconds and remove NaNs
-        return list(np.array([y for y in y_data if y != float('nan')]) * 1e3)
+        # TODO(weberlo): this is a garbage way to deal with NaNs
+        # seconds to milliseconds and replace NaNs with the minimum y value
+        min_y = min(filter(lambda y: not np.isnan(y), y_data))
+        return list(np.array([min_y if np.isnan(y) else y for y in y_data]) * 1e3)
 
     def _post_bar_setup(self, y_data):
         # only use a grid on the y axis
@@ -219,16 +220,18 @@ class PlotBuilder:
                          ha='center', va='bottom')
 
     def _set_up_y_axis(self, y_data):
+        # TODO(weberlo): Refactor `_choose_linear_step` and `_choose_log_base`.
         def _choose_linear_step(y_min, y_max):
-            best_step = LINEAR_AXIS_STEPS[0]
+            best_step = None
             best_step_start = None
             best_step_stop = None
             min_diff = float('inf')
             for step in LINEAR_AXIS_STEPS:
                 step_start = int(y_min / step) * step
                 step_stop = int((y_max / step) + 1) * step
-                diff = abs(((step_stop - step_start) / step) - self.num_y_ticks)
-                if diff <= min_diff:
+                num_y_ticks = (step_stop - step_start) / step
+                diff = abs(num_y_ticks - TARGET_NUM_Y_TICKS)
+                if diff <= min_diff and MIN_NUM_Y_TICKS < num_y_ticks < MAX_NUM_Y_TICKS:
                     best_step = step
                     best_step_start = step_start
                     best_step_stop = step_stop
@@ -236,19 +239,22 @@ class PlotBuilder:
             return best_step, best_step_start, best_step_stop
 
         def _choose_log_base(y_min, y_max):
-            best_base = LOG_AXIS_BASES[0]
+            best_base = None
             best_base_start = None
             best_base_stop = None
             min_diff = float('inf')
             for base in LOG_AXIS_BASES:
                 base_start = int(np.log(y_min) / np.log(base))
                 base_stop = int((np.log(y_max) / np.log(base)) + 1)
-                diff = abs((base_stop - base_start) - self.num_y_ticks)
-                if diff <= min_diff:
+                num_y_ticks = base_stop - base_start
+                diff = abs(num_y_ticks - TARGET_NUM_Y_TICKS)
+                if diff <= min_diff and MIN_NUM_Y_TICKS < num_y_ticks < MAX_NUM_Y_TICKS:
                     best_base = base
                     min_diff = diff
                     best_base_start = base_start
                     best_base_stop = base_stop
+            if best_base_start is None:
+                raise RuntimeError('no suitable log base could be found for visualization')
             return best_base, best_base_start, best_base_stop
 
         y_data = list(y_data)
@@ -270,10 +276,12 @@ class PlotBuilder:
         y_max = max(y_data)
         if y_scale == PlotScale.LINEAR:
             step, start, stop = _choose_linear_step(y_min, y_max)
-            self.ax.set_yticks([i for i in range(start, stop+1, step)])
+            if step is not None:
+                self.ax.set_yticks([i for i in np.arange(start, stop, step)])
         elif y_scale == PlotScale.LOG:
             base, start, stop = _choose_log_base(y_min, y_max)
-            self.ax.set_yticks([base**i for i in range(start, stop+1)])
+            if base is not None:
+                self.ax.set_yticks([base**i for i in range(start, stop+1)])
 
         if self.plot_type == PlotType.MULTI_BAR:
             # add padding on the top to make room for bar labels
@@ -288,10 +296,6 @@ class PlotBuilder:
         outfile = prepare_out_file(dirname, filename)
         plt.savefig(outfile, dpi=500, bbox_inches='tight')
         plt.close()
-
-    def set_style(style_name):
-        self.style = style_name
-        return self
 
     def set_title(self, title):
         self.title = title
