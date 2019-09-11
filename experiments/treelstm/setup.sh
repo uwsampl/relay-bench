18c893b0774fc3ed11002b3b3ed356c02771375e
Steven S. Lyubomirsky <sslyu@cs.washington.edu>
2019-09-11 15:06:01 -0700

    Have the treelstm setup clean up installed files properly

--- experiments/treelstm/setup.sh
+++ experiments/treelstm/setup.sh
@@ -7,6 +7,12 @@ source $BENCHMARK_DEPS/bash/common.sh
 include_shared_python_deps
 add_to_pythonpath $(pwd)
 
+# ensure the setup scripts are in a fresh state
+rm -rf pt_tlstm/checkpoints
+rm -rf pt_tlstm/data
+rm -rf pt_tlstm/lib/stanford*
+rm -rf pt_tlstm/lib/*.class
+
 cp -r pt_tlstm "$setup_dir"
 cd "$setup_dir/pt_tlstm"
 wrap_script_status "$setup_dir" "fetch_and_preprocess.sh"
